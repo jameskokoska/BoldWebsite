f96ce13c32960efa28c8b9f79095d084cb4920c3
jameskokoska <jameskokoska@users.noreply.github.com>
2020-11-05 01:03:23 -0500

    changes some front-end and data is read from data file

---
+++ data.js
@@ -0,0 +1,23 @@
+var name = "John Smiths";
+var aboutContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam a cursus tortor, id fringilla lectus. Mauris quis volutpat nunc. Maecenas mattis feugiat enim, et maximus orci vulputate sit amet. Aliquam aliquam ultrices odio. Nullam iaculis, tortor ut porta dapibus, lacus diam volutpat diam, nec molestie diam mi ac augue. Vivamus suscipit venenatis sem, id molestie nunc mollis et. In vitae sodales est, at lobortis lorem.";
+var experienceContent = [
+    "Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent vestibulum orci vitae orci malesuada, ac placerat nulla feugiat. Phasellus aliquet, nisl eu ornare pharetra, metus arcu rhoncus est, vitae varius nunc libero in leo. Nunc turpis erat, accumsan et lorem vitae, pulvinar consequat orci. Donec mollis placerat tortor, quis dignissim lacus sagittis ut. Morbi at eros ac ante semper ultrices. Sed ligula tellus, laoreet a iaculis rhoncus, iaculis eu augue. Interdum et malesuada fames ac ante ipsum primis in faucibus.",
+    "Nulla augue risus, mattis eget maximus ut, ultricies pellentesque libero. Quisque non venenatis metus, ut auctor nisl. Donec elementum quam eget nulla ornare, ut condimentum tortor faucibus. Sed fermentum sit amet quam vitae lacinia. Duis porttitor quam est, et bibendum nisi egestas quis. Phasellus aliquet vehicula vehicula.",
+    "Nulla libero augue, fermentum nec imperdiet vel, sagittis a nisl. Vivamus nibh est, efficitur quis auctor eu, auctor vel erat. Nunc non ante felis. Duis posuere, ligula non malesuada sagittis, diam turpis sodales massa, id consectetur turpis turpis at lorem. Suspendisse pretium metus vel nibh tempor, a auctor mauris placerat. Cras at fermentum risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultrices lorem justo, eget euismod mauris vulputate id. Mauris rhoncus, mi id maximus placerat, velit massa blandit est, id finibus urna lacus quis dolor.",
+    "Nulla porta quis magna vitae lobortis. Vestibulum cursus nunc id dui semper, et placerat ligula porta. Morbi eget nulla at ex blandit ullamcorper. Curabitur nec ornare nisi. Quisque sed imperdiet lorem. Aliquam at metus vel mauris tincidunt viverra. Nullam a viverra sem.",
+];
+var contactContent = "Donec enim odio, tempus vel venenatis sed, gravida ut justo. Ut ornare mattis neque fringilla dictum. Vestibulum nec urna eros. Quisque lacinia diam et convallis dictum."
+var contact = [
+    {
+        "fontawesomeCode":"fab fa-linkedin-in",
+        "link":"https://ca.linkedin.com/",
+    },
+    {
+        "fontawesomeCode":"fab fa-facebook-f",
+        "link":"https://www.facebook.com/",
+    },
+    {
+        "fontawesomeCode":"fas fa-envelope",
+        "link":"https://www.google.com/",
+    },
+];
